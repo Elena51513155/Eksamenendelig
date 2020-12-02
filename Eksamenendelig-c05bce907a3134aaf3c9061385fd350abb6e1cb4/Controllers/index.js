622001f2f2b13787928c925e80866815e3051c48
Elena Frees <elfr20ab@student.cbs.dk>
2020-12-02 16:26:57 +0100

    change

--- Eksamenendelig-c05bce907a3134aaf3c9061385fd350abb6e1cb4/Controllers/index.js
+++
@@ -1,8 +0,0 @@
-//her håndterer vi menupunkter
-
-var path = require("path");
-
-exports.frontpage_get = function(req, res) {
-    res.sendFile(path.join(__dirname + "/../views/register.html"));
-
-};
